22bb1328a06f9908bed4833822cdda55f8e82ccc
Krzysztof Wilczynski <kw@linux.com>
2017-09-21 14:00:56 +0100

    Support SSH key passed as a variable.
    Signed-off-by: Krzysztof Wilczynski <kw@linux.com>

--- modules/aws/key-pair/scripts/public-key.sh
+++ modules/aws/key-pair/scripts/public-key.sh
@@ -4,12 +4,23 @@ set -e
 set -u
 set -o pipefail
 
+print_public_key() {
+    local key=$1
+
+    if [[ -n "$key" ]]; then
+        jq -c -n --arg public_key "$key" '{ "public_key": $public_key }'
+    else
+        echo '{"public_key":""}'
+    fi
+}
+
 export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
+PUBLIC_KEY=${PUBLIC_KEY-}
 eval "$(jq -r '@sh "PUBLIC_KEY=\(.public_key)"')"
 
 if [[ -f $PUBLIC_KEY ]]; then
-    jq -c -n --arg public_key "$(cat ${PUBLIC_KEY})" '{ "public_key": $public_key }'
+    print_public_key "$(cat ${PUBLIC_KEY})"
 else
-    echo '{ "public_key": "" }'
+    print_public_key "$(echo ${PUBLIC_KEY})"
 fi
